11468125f15f95c999c0abf091bacc09c1ab6001
rimaz523 <rimazmohommed523@gmail.com>
2017-06-10 19:55:16 +0530

    introducing twigs

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,13 +3,14 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 //use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        $params = array('name' => $name);
+class DefaultController extends Controller {
+
+    public function indexAction($count, $name) {
+        $params = array('name' => $name, 'count' => $count);
         return $this->render('EventBundle:Default:index.html.twig', $params);
     }
+
 }
